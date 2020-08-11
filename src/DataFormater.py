1bf87deaf4346b44c6ade4d6208fc11e5968b7bf
Aidan-Jared <AidanJared42@gmail.com>
2020-08-10 18:50:24 -0600

    changed how the match data is saved

--- src/DataFormater.py
+++ src/DataFormater.py
@@ -1,25 +1,30 @@
 import json
 import time
 import requests
+import pandas as pd
 
 if __name__ == "__main__":
+    # read in all matches from json
     input_file = open("data/liquid_matches.json")
     matches = json.load(input_file)
     input_file.close()
     print('Loading {} matches\n'.format(len(matches)))
 
-    with open('data/liquid_match_data.txt', 'w', encoding="utf-8") as f:
-        i = 0
-        for m in matches:
-            if i == 100:
-                break
-            print('requseting match {}'.format(m["match_id"]))
-            r = requests.get('https://api.opendota.com/api/matches/{}'.format(m['match_id']))
-            f.write(r.text)
-            f.write('\n')
-            print(r.text)
-            time.sleep(5)
-            i += 1
+    # save match data to local machine
+    i = 0
+    df = pd.DataFrame()
+    for m in matches:
+        if i == 100:
+            # limmit number of matches read in
+            break
+        # get match data from api
+        print('requseting match {}'.format(m["match_id"]))
+        r = requests.get('https://api.opendota.com/api/matches/{}'.format(m['match_id']))
+        match = r.json()
+        picks_bans = match['picks_bans']
+        df = df.append(picks_bans, ignore_index = True)
 
-        print('processed {} matches'.format(i))
-    f.close()
+        # prevent request errors
+        time.sleep(5)
+        i += 1
+    print('processed {} matches'.format(i))
