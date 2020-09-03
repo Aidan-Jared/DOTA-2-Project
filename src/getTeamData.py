69cb7062481c7d2dfcd628dd2e0fd79524340b7d
Aidan-Jared <AidanJared42@gmail.com>
2020-09-02 19:53:09 -0600

    fixed bugs preventing dataframes from merging

--- src/getTeamData.py
+++ src/getTeamData.py
@@ -29,29 +29,29 @@ def getWinner(match, df):
     dire_team = match['dire_team']
     radiant_team = match['radiant_team']
     if radiant_win == True:
-        win = {"winner" : radiant_team['name'], "match_id" : str(match['match_id'])}
+        win = {"winner" : radiant_team['name'], "match_id" : str(match['match_id'])} # turn to string to prevent from turining into scientific notation
     elif radiant_win == False:
         win = {"winner" : dire_team['name'], "match_id" : str(match['match_id'])}
     df = df.append(win, ignore_index = True)
     return df
 
-# def windeltaPressence(df_picks, df_win, team, heroes):
-#     # merge win data frame with the picks for the match and isolate a team
-#     df_picks_wins = df_picks.merge(df_win, how='left', left_on='match_id', right_on='match_id')
-#     df_team_picks = df_picks_wins[df_picks_wins['team'] == team]
+def windeltaPressence(df_picks, df_win, team, heroes):
+    # merge win data frame with the picks for the match and isolate a team
+    df_picks_wins = df_picks.merge(df_win, how='left', left_on='match_id', right_on='match_id')
+    df_team_picks = df_picks_wins[df_picks_wins['team'] == team]
 
-#     # get number of wins and losses
-#     team_loss = df_team_picks[df_team_picks.winner != team].hero_id.value_counts().rename('number_of_losses')
-#     team_win = df_team_picks[df_team_picks.winner == team].hero_id.value_counts().rename('number_of_wins')
+    # get number of wins and losses
+    team_loss = df_team_picks[df_team_picks.winner != team].hero_id.value_counts().rename('number_of_losses')
+    team_win = df_team_picks[df_team_picks.winner == team].hero_id.value_counts().rename('number_of_wins')
 
-#     # merge wins and losses to all the heroes in the game
-#     df_win_loss = heroes.merge(team_loss.to_frame(), how='left', left_on='name', right_index=True, ).merge(team_win.to_frame(), how='left', left_on='name', right_index=True).fillna(0)
+    # merge wins and losses to all the heroes in the game
+    df_win_loss = heroes.merge(team_loss.to_frame(), how='left', left_on='name', right_index=True, ).merge(team_win.to_frame(), how='left', left_on='name', right_index=True).fillna(0)
 
-#     # calculate win delta and pressence, remove characters that where never played
-#     df_win_loss['win_delta'] = (df_win_loss['number_of_wins'] - df_win_loss['number_of_losses']) / len(df_win)
-#     df_win_loss['pressence'] = (df_win_loss['number_of_wins'] + df_win_loss['number_of_losses']) / len(df_win)
-#     df_win_loss = df_win_loss[df_win_loss['pressence'] > 0]
-#     return df_win_loss
+    # calculate win delta and pressence, remove characters that where never played
+    df_win_loss['win_delta'] = (df_win_loss['number_of_wins'] - df_win_loss['number_of_losses']) / len(df_win)
+    df_win_loss['pressence'] = (df_win_loss['number_of_wins'] + df_win_loss['number_of_losses']) / len(df_win)
+    df_win_loss = df_win_loss[df_win_loss['pressence'] > 0]
+    return df_win_loss
 
 def pullPicksBans(matches, number = 5):
     if type(matches) != list:
@@ -76,7 +76,8 @@ def pullPicksBans(matches, number = 5):
         i += 1
     df_pb['hero_name'] = df_pb['hero_id']
     df_pb = df_pb.replace({'hero_name' : names})
-    # df_winD = windeltaPressence(df_pb, df_win, team_name, df_heroes)
+    df_win['match_id'] = df_win['match_id'].astype('int64')
+    df_winD = windeltaPressence(df_pb, df_win, team_name, df_heroes)
     return df_pb, df_win, df_winD
 
 if __name__ == "__main__":
